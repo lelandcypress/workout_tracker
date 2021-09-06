31581133a6f06bbcfe5b350118686145b55e99e4
Michael Souter <souterm1@gmail.com>
2021-09-05 23:05:02 -0400

    created server.js and routes.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+//const routes = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+//app.use(routes);
+
+app.listen(PORT, () => console.log("Now listening"));
