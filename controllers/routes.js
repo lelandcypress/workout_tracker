b933f36882c881aa25cbebcda89a0b1aa1bdf8ec
Michael Souter <souterm1@gmail.com>
2021-09-06 23:30:25 -0400

    seed run successfully, started on route building

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,3 +1,21 @@
 const router = require("express").Router();
+const Workout = require("../models/workout");
+
+router.get("/exercise", (req, res) => {
+  res.redirect("exercise.html");
+});
+
+router.get("/stats", (req, res) => {
+  res.redirect("stats.html");
+});
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    const workout = await Workout.find({}).sort({ date: -1 });
+    res.json(workout);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
